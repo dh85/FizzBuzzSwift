76e14ad7cc18cdb46abc9f76d23064eedebec13b
David Hughes <39618866+dh85@users.noreply.github.com>
2023-07-20 11:31:46 +0100

    Final refactoring

--- Sources/FizzBuzzSwift/FizzBuzzSwift.swift
+++ Sources/FizzBuzzSwift/FizzBuzzSwift.swift
@@ -1,12 +1,8 @@
 func fizzBuzz(input: Int) -> String {
-    if input.isMultiple(of: 3) && input.isMultiple(of: 5) {
-        return "FizzBuzz"
+    switch (input.isMultiple(of: 3), input.isMultiple(of: 5)) {
+    case (true, false): return "Fizz"
+    case (false, true): return "Buzz"
+    case (true, true): return "FizzBuzz"
+    case (false, false): return String(input)
     }
-    if input.isMultiple(of: 3) {
-        return "Fizz"
-    }
-    if input.isMultiple(of: 5) {
-        return "Buzz"
-    }
-    return String(input)
 }
